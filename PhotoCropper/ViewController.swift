baf71c1fd5020e9fa54f0c7b86a441fec8fcb23d
FengYQ <yiqi@xmind.org>
2021-03-02 16:43:18 +0800

    Initial Commit

---
+++ PhotoCropper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoCropper
+//
+//  Created by Edmund Feng on 2021/3/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
