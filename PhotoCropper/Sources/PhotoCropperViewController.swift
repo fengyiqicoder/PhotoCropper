0d5f7de8f1bfd2304fe2faf15d79e0dfd138693b
FengYQ <yiqi@xmind.org>
2021-03-03 21:22:40 +0800

    cropper is done

--- PhotoCropper/Sources/PhotoCropperViewController.swift
+++ PhotoCropper/Sources/PhotoCropperViewController.swift
@@ -7,45 +7,194 @@
 
 import UIKit
 
-enum Mode {
-    case camera, image
+protocol PhotoCropperDelegate: class {
+    func didCrop(image: UIImage)
 }
 
 class PhotoCropperViewController: UIViewController {
-    
+    weak var delegate: PhotoCropperDelegate!
+
+    enum Mode {
+        case camera, crop
+    }
+
+    static func instance() -> PhotoCropperViewController {
+        let vc = UIStoryboard(name: "PhotoCropper", bundle: nil).instantiateInitialViewController()! as! PhotoCropperViewController
+        return vc
+    }
+
     func configAfterInit() {
         modalPresentationStyle = .overFullScreen
         modalTransitionStyle = .crossDissolve
+        mode = .crop
     }
-    
-    @IBOutlet weak var cropperView: UIView!
-    @IBOutlet weak var controlView: UIView!
-    
-    @IBOutlet weak var snap: UIButton!
-    @IBOutlet weak var leftButton: UIButton!
-    @IBOutlet weak var rightButton: UIButton!
-    @IBOutlet weak var hint: UILabel!
-    @IBOutlet weak var imageView: UIImageView!
-    
+
+    var image: UIImage?
+
+    @IBOutlet var cropperView: UIView!
+    @IBOutlet var controlView: UIView!
+
+    @IBOutlet var snap: UIButton!
+    @IBOutlet var leftButton: UIButton!
+    @IBOutlet var rightButton: UIButton!
+    @IBOutlet var hint: UILabel!
+    @IBOutlet var imageView: UIImageView!
+
+    var sideButtons: [UIButton] {
+        [leftButton, rightButton]
+    }
+
     var mode: Mode!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        sideButtons.forEach {
+            $0.contentEdgeInsets = UIEdgeInsets(top: 6, left: 8, bottom: 6, right: 8)
+            $0.layer.cornerRadius = 8
+            $0.layer.cornerCurve = .continuous
+        }
+
         if case .camera = mode {
-            snap.isHidden = false
-            leftButton.setTitle("Cancel", for: .normal)
-            rightButton.setImage(UIImage(systemName: "bolt.badge.a.fill"), for: .normal)
-            
-            //add camera view
+            initCameraControlView()
+
+            // add camera view
         } else {
-            snap.isHidden = true
-            leftButton.setTitle("Cancel", for: .normal)
-            rightButton.setTitle("Done", for: .normal)
-            
-            //config imageView
+            initCropModeControlView()
+            hint.text = canCropHint
+            // config imageView
+            imageView.image = image
+
+            // addGestureRecoginzer
+            cropperView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(selectingImage(sender:))))
         }
     }
-    
-    
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+
+    fileprivate func initCropModeControlView() {
+        snap.isHidden = true
+        leftButton.setTitle("Cancel", for: .normal)
+        rightButton.setTitle("Done", for: .normal)
+    }
+
+    fileprivate func initCameraControlView() {
+        snap.isHidden = false
+        leftButton.setTitle("Cancel", for: .normal)
+        rightButton.setImage(UIImage(systemName: "bolt.badge.a.fill"), for: .normal)
+    }
+
+    // Gestrue
+
+    @objc
+    func selectingImage(sender: UIPanGestureRecognizer) {
+        let location = sender.location(in: cropperView)
+
+        if sender.state == .began {
+            addCropper(at: location)
+        }
+
+        if sender.state == .changed || sender.state == .ended {
+            changeCropper(rightDownPoint: location)
+            hint.text = hasCropperHint
+        }
+    }
+
+    var cropper: UIView?
+
+    func addCropper(at location: CGPoint) {
+        cropper?.removeFromSuperview()
+        cropper = nil
+        cropper = UIView()
+
+        let cropper = self.cropper!
+        cropper.layer.borderWidth = 1.8
+        cropper.layer.cornerRadius = 2
+        cropper.layer.borderColor = UIColor.red.cgColor
+        cropper.frame = CGRect(origin: location, size: CGSize.zero)
+        cropperView?.addSubview(cropper)
+    }
+
+    func changeCropper(rightDownPoint: CGPoint) {
+        guard let cropper = self.cropper else { return }
+        let width = rightDownPoint.x - cropper.frame.origin.x
+        let height = rightDownPoint.y - cropper.frame.origin.y
+        cropper.frame.size.width = width < 0 ? 0 : width
+        cropper.frame.size.height = height < 0 ? 0 : height
+    }
+
+    // MARK: - Action
+
+    @IBAction func cancal() {
+        dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func done() {
+        guard let image = cropImage() else { return }
+        delegate?.didCrop(image: image)
+        dismiss(animated: true, completion: nil)
+    }
+
+    func cropImage() -> UIImage? {
+        guard let croperRect = cropper?.frame else { return nil }
+        var rectInImageView = cropperView.convert(croperRect, to: imageView)
+
+        rectInImageView.origin.x -= imageView.contentClippingRect.origin.x
+        rectInImageView.origin.y -= imageView.contentClippingRect.origin.y
+
+        let croppedImage = cropImage(imageView.image!, toRect: rectInImageView, from: imageView)
+        return croppedImage
+    }
+
+    // MARK: - hint
+
+    let canCropHint = "框选想要的图片区域"
+    let hasCropperHint = "重新框选或点击完成"
+
+    func cropImage(_ inputImage: UIImage, toRect cropRect: CGRect, from imageView: UIImageView) -> UIImage? {
+        let viewWidth = imageView.frame.width
+        let viewHeight = imageView.frame.height
+
+        let imageViewScale = max(inputImage.size.width / viewWidth,
+                                 inputImage.size.height / viewHeight)
+
+        // Scale cropRect to handle images larger than shown-on-screen size
+        let cropZone = CGRect(x: cropRect.origin.x * imageViewScale,
+                              y: cropRect.origin.y * imageViewScale,
+                              width: cropRect.size.width * imageViewScale,
+                              height: cropRect.size.height * imageViewScale)
+
+        // Perform cropping in Core Graphics
+        guard let cutImageRef: CGImage = inputImage.cgImage?.cropping(to: cropZone)
+        else {
+            return nil
+        }
+
+        // Return image to UIImage
+        let croppedImage: UIImage = UIImage(cgImage: cutImageRef)
+        return croppedImage
+    }
+}
+
+fileprivate extension UIImageView {
+    var contentClippingRect: CGRect {
+        guard let image = image else { return bounds }
+        guard contentMode == .scaleAspectFit else { return bounds }
+        guard image.size.width > 0 && image.size.height > 0 else { return bounds }
+
+        let scale: CGFloat
+        if image.size.width >= image.size.height {
+            scale = bounds.width / image.size.width
+        } else {
+            scale = bounds.height / image.size.height
+        }
+
+        let size = CGSize(width: image.size.width * scale, height: image.size.height * scale)
+        let x = (bounds.width - size.width) / 2.0
+        let y = (bounds.height - size.height) / 2.0
+
+        return CGRect(x: x, y: y, width: size.width, height: size.height)
+    }
 }
--- PhotoCropper/ViewController.swift
+++ PhotoCropper/ViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func test(_ sender: Any) {
+       showImagePicker()
+    }
+    
+}
 
+extension ViewController: UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+    func showImagePicker() {
+        let picker = UIImagePickerController()
+        picker.sourceType = .photoLibrary
+        picker.delegate = self
+        self.present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.originalImage] as? UIImage else { return }
+        picker.dismiss(animated: true) {
+            let vc = PhotoCropperViewController.instance()
+            vc.image = image
+            self.present(vc, animated: true, completion: nil)
+        }
+    }
 }
 
